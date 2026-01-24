a04c958d743f0b44ce27dd60943f2e86eb8a796d
sarah-analytics <sarahj0514@gmail.com>
2026-01-24 02:53:43 -0800

    Update kpi_02_daily_revenue.sql

--- sql/kpi/kpi_02_daily_revenue.sql
+++ sql/kpi/kpi_02_daily_revenue.sql
@@ -18,17 +18,16 @@ WITH params AS (
         TIMESTAMP('2017-01-01 00:00:00') AS start_ts,
         TIMESTAMP('2018-01-01 00:00:00') AS end_ts
 )
-
 SELECT
-    DATE(o.order_purchase_timestamp)      AS order_date,     -- day grain
-    SUM(p.payment_value)                  AS daily_revenue   -- total revenue per day
+    DATE(o.order_purchase_timestamp) AS order_date,      -- day grain
+    SUM(p.payment_value)            AS daily_revenue     -- total revenue per day
 FROM orders AS o
-JOIN params AS prm
-  ON o.order_purchase_timestamp >= prm.start_ts
- AND o.order_purchase_timestamp <  prm.end_ts
 JOIN order_payments AS p
-  ON o.order_id = p.order_id              -- many-side join (payments)
-   GROUP BY
+  ON p.order_id = o.order_id
+CROSS JOIN params AS prm
+WHERE o.order_purchase_timestamp >= prm.start_ts
+  AND o.order_purchase_timestamp <  prm.end_ts
+GROUP BY
     DATE(o.order_purchase_timestamp)
 ORDER BY
     order_date;
